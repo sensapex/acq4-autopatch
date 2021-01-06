41de8357bdc4455fff2e6bbb35a9f32f8f71a87e
Martin <outofculture@gmail.com>
2021-01-05 23:28:58 -0800

    polar coors could work

--- acq4_autopatch/job_queue.py
+++ acq4_autopatch/job_queue.py
@@ -2,14 +2,22 @@ import numpy as np
 from acq4.util.Mutex import Mutex
 
 
+def _polar2z(r, theta):
+    return r * np.exp(1j * theta)
+
+
+def _z2polar(z):
+    return (np.abs(z), np.angle(z))
+
+
 class JobQueue(object):
     """Stores a list of jobs and assigns them by request.
 
     Pipettes are selected for each job based on the quadrant that the target appears in.
     """
 
-    def __init__(self, pipettes, module):
-        self.pipettes = pipettes  # {pipette_name: quadrant}
+    def __init__(self, patch_device_names, module):
+        self.pipettes = patch_device_names
         self.module = module
         self.protocol = None
         self.all_jobs = []
@@ -46,24 +54,31 @@ class JobQueue(object):
             if len(self.queued_jobs) == 0:
                 return None
 
-            # current pipette position
-            pos = np.array(pipette.pipetteDevice.globalPosition())
+            boundaries = self.module.boundaries_by_pipette[patch_pipette.pipetteDevice]
+            # TODO consider the degenerate case: only one pipette
+            # TODO consider the semi-degenerate case: only two pipettes
+            # TODO the center of the jobs maybe needs to account for all currenttly active jobs
 
-            # all job positions
-            positions = np.array([job.position for job in self.queued_jobs])
+            # # current pipette position
+            # pos = np.array(patch_pipette.pipetteDevice.globalPosition())
+            #
+            # # all job positions
+            # positions = np.array([job.position for job in self.queued_jobs])
+            #
+            # # which quadrant does this pipette belong in?
+            # pip_quad = np.array(self.pipettes[patch_pipette.name()]).astype(bool)
+            #
+            # # mask cells that are not in the same quadrant
+            # quad_center = np.array(self.center[:2])
+            # cell_quads = positions[:, :2] > quad_center
+            # quad_mask = (cell_quads == pip_quad[None, :]).all(axis=1)
 
-            # which quadrant does this pipette belong in?
-            pip_quad = np.array(self.pipettes[pipette.name()]).astype(bool)
+            positions = np.array([_z2polar(job.position) for job in self.queued_jobs])
 
-            # mask cells that are not in the same quadrant
-            quad_center = np.array(self.center[:2])
-            cell_quads = positions[:, :2] > quad_center
-            quad_mask = (cell_quads == pip_quad[None, :]).all(axis=1)
-
-            # find closest cell to this pipette, excluding other quads
+            # find closest cell to this pipette, excluding other slices
             diff = positions - np.array(pos).reshape(1, 3)
             dist = (diff ** 2).sum(axis=1) ** 0.5
-            dist[~quad_mask] = np.inf
+            # dist[~quad_mask] = np.inf
             closest = np.argmin(dist)
             if dist[closest] == np.inf:
                 return None
